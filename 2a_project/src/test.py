fccfb26bb625cc5555059d0edea6208fb65c5cb1
Hudson Kortus <hudsonwkortus@gmail.com>
2025-09-23 18:45:22 -0400

    fixed and tested is_line collion free

--- 2a_project/src/test.py
+++ 2a_project/src/test.py
@@ -4,5 +4,5 @@ from environment import Environment3D
 if __name__ == "__main__":
     env = Environment3D()
     env.parse_map_file(filename='./maps/map4.txt')
-    env.is_point_in_free_space([2,2,2])
-    env.is_line_collision_free([0,0,0],[.5,0.5,0])
+    env.is_point_in_free_space([12,12,12])
+    print(f" line is collision free: {env.is_line_collision_free([0,0,0],[-6,-6,-6])}")
