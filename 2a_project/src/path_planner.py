e2b286b70d3135cd1d283d5501caee4195db69b7
Hudson Kortus <hudsonwkortus@gmail.com>
2025-09-24 21:12:47 -0400

    implemented and lightly tested RRTstar

--- 2a_project/src/path_planner.py
+++ 2a_project/src/path_planner.py
@@ -1,13 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+from typing import List
+
+Position = np.ndarray
 
 class RRTNode:
     """Node for RRT* tree"""
     def __init__(self, position, parent=None):
-        self.position = np.array(position, dtype=float)
-        self.parent = parent
-        self.cost = 0.0
+        self.position: Position = np.array(position, dtype=float)
+        self.parent:RRTNode|None = parent
+        self.cost:float = 0.0
         self.children = []
 
 class PathPlanner:
@@ -18,7 +21,7 @@ class PathPlanner:
     def __init__(self, environment):
         self.env = environment
         self.waypoints = []
-        self.tree_nodes = []
+        self.tree_nodes:List[RRTNode] = []
         
         # RRT* parameters
         self.max_iterations = 3000
@@ -28,15 +31,92 @@ class PathPlanner:
         self.goal_bias = 0.15  # 15% bias towards goal
         
     
-    ############################################################################################################
-    #### TODO - Implement RRT* path planning algorithm in 3D (use the provided environment class) ##############
-    
+    def plan(self):
+        root=RRTNode(self.env.start_point)
+        self.tree_nodes.append(root)
+
+        for i in range(self.max_iterations):
+            if np.random.rand() < self.goal_bias:
+                random_point: Position = self.env.goal_point
+            else:
+                random_point: Position = self.env.generate_random_free_point()
+            
+            nearest_node: RRTNode = self.get_nearest_node(node_list=self.tree_nodes, point=random_point)
+            node_position: Position = self.step(start=nearest_node.position,end=random_point)
+            
+            if self.env.is_line_collision_free(p1=nearest_node.position, p2=node_position):
+                neighbors: List[RRTNode] = self.get_neighbors(node_list=self.tree_nodes, point=node_position)
+                new_node:RRTNode = self.set_best_parent(neighbors=neighbors,nearest_node=nearest_node,new_position=node_position)
+                self.tree_nodes.append(new_node)
+                self.rewire(neighbors=neighbors, new_node=new_node)
+
+                if self.reached_goal(new_node=new_node):
+                    self.save_path_to_waypoint(final_node=new_node)
+                    self.waypoints = self.save_path_to_waypoint(final_node=new_node)
+                    return True
+
     #### TODO - Store the final path in self.waypoints as a list of 3D points ##################################
+    def save_path_to_waypoint(self, final_node:RRTNode):
+        final_path:List[Position] = []
+        node:RRTNode = final_node
+        while node is not None:
+            final_path.append(node.position)
+            node = node.parent
+        return final_path[::-1]
+
     #### TODO - Add member functions as needed #################################################################
-    ############################################################################################################
 
+    def get_nearest_node(self, node_list: List[RRTNode], point: Position):
+        distances = []
+        for node in node_list:
+            distance = np.linalg.norm(node.position - point)
+            distances.append(distance)
+        nearest_idx = np.argmin(distances)
+        return node_list[nearest_idx]
 
+    def step(self, start: Position, end: Position):
+        direction = end - start
+        distance = np.linalg.norm(direction)
+        if distance <= self.step_size:
+            return end
+        unit_direction = direction / distance
+        node_position = start + self.step_size * unit_direction
+        return node_position
     
+    def get_neighbors(self, node_list: List[RRTNode], point: Position):
+        neighbors: List[RRTNode] = []
+        for node in node_list:
+            if np.linalg.norm(node.position - point) <= self.search_radius:
+                neighbors.append(node)
+        return neighbors
+
+    def set_best_parent(self, neighbors: List[RRTNode], nearest_node: RRTNode, new_position: Position):
+        best_parent: RRTNode = nearest_node
+        best_cost = nearest_node.cost + np.linalg.norm(nearest_node.position - new_position)
+        for neighbor in neighbors:
+            neighbor_to_position_distance = np.linalg.norm(neighbor.position - new_position)
+            if neighbor.cost + neighbor_to_position_distance <= best_cost:
+                best_parent = neighbor
+                best_cost = neighbor.cost + neighbor_to_position_distance
+        new_node = RRTNode(position=new_position, parent=best_parent)
+        new_node.cost = best_cost
+        return new_node
+
+    def rewire(self, neighbors: List[RRTNode], new_node: RRTNode):
+        for neighbor in neighbors:
+            if neighbor == new_node.parent:  # Skip parent
+                continue
+            new_cost = new_node.cost + np.linalg.norm(neighbor.position - new_node.position)
+            if new_cost < new_node.cost and self.env.is_line_collision_free(p1=neighbor.position,p2=new_node.position):
+                neighbor.parent = new_node
+                neighbor.cost = new_cost
+
+    def reached_goal(self, new_node: RRTNode):
+        distance_error = np.linalg.norm(new_node.position - self.env.goal_point)
+        if distance_error <= self.goal_radius:
+            return True
+
+    ############################################################################################################
     def visualize_tree(self, ax=None):
         """Visualize the RRT* tree"""
         if ax is None:
@@ -45,7 +125,10 @@ class PathPlanner:
             standalone = True
         else:
             standalone = False
-        
+
+        ax.plot(self.env.start_point[0],self.env.start_point[1], self.env.start_point[2], 'ro-', markersize=12, linewidth=3, label='Start')
+        ax.plot(self.env.goal_point[0],self.env.goal_point[1], self.env.goal_point[2], 'ro-', markersize=12, linewidth=3, label='End')
+
         # Draw tree edges
         for node in self.tree_nodes:
             if node.parent is not None:
