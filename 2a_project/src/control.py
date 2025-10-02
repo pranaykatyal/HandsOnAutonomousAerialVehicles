e7e171ac92f60800acc283c8493aaffd9fce34f4
Pranay Katyal <pkatyal@wpi.edu>
2025-10-01 21:49:12 -0400

    finetuned

--- 2a_project/src/control.py
+++ 2a_project/src/control.py
@@ -83,7 +83,7 @@ class quad_control:
     def __init__(self):
 
         # TODO - SET CONTROLLER PROPERTIES AND GAINS 
-        dt = 0.02
+        dt = 0.025
         filter_tau = 0.1
         self.dt = dt
 
@@ -107,17 +107,17 @@ class quad_control:
         ########################### TODO - SET GAINS HERE ###########################
         # EDIT PID GAINS HERE! (kp, ki, kd, filter_tau, dt, dim = 1, minVal = -1, maxVal = 1)
         # NED position controller. EDIT GAINS HERE
-        self.x_pid = pid(0.4, 0.0, 0.2, filter_tau, dt, minVal = minVel, maxVal=maxVel)
-        self.y_pid = pid(0.4, 0.0, 0.2, filter_tau, dt, minVal = minVel, maxVal=maxVel)
-        self.z_pid = pid(0.8, 0.0, 0.25, filter_tau, dt, minVal = minVel, maxVal=maxVel)
+        self.x_pid = pid(0.8, 0.0, 0.2, filter_tau, dt, minVal = minVel, maxVal=maxVel)
+        self.y_pid = pid(0.8, 0.0, 0.2, filter_tau, dt, minVal = minVel, maxVal=maxVel)
+        self.z_pid = pid(1.0, 0.0, 0.25, filter_tau, dt, minVal = minVel, maxVal=maxVel)
 
 
         ########################## TODO - SET GAINS HERE ##############################
         ###############################################################################
         # NED velocity controller. EDIT GAINS HERE
-        self.vx_pid = pid(0.5, 0.0, 0.3, filter_tau, dt, minVal = minAcc, maxVal=maxAcc)
-        self.vy_pid = pid(0.5, 0.0, 0.3, filter_tau, dt, minVal = minAcc, maxVal=maxAcc)
-        self.vz_pid = pid(1.0, 0.0, 0.4, filter_tau, dt, minVal = minAcc, maxVal=maxAcc)
+        self.vx_pid = pid(1.5, 0.1, 0.3, filter_tau, dt, minVal = minAcc, maxVal=maxAcc)
+        self.vy_pid = pid(1.5, 0.1, 0.3, filter_tau, dt, minVal = minAcc, maxVal=maxAcc)
+        self.vz_pid = pid(2.0, 0.2, 0.4, filter_tau, dt, minVal = minAcc, maxVal=maxAcc)
         ##############################################################################
         ##############################################################################
 
