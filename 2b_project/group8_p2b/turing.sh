f332bcc4ce24b4043ffbb91e5e20222af99fe445
Pranay Katyal <pkatyal@login-02.cm.cluster>
2025-10-22 15:34:54 -0400

    adding the turing update

--- 2b_project/group8_p2b/turing.sh
+++ 2b_project/group8_p2b/turing.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
 
-#SBATCH --mail-user=hkortus@wpi.edu
+#SBATCH --mail-user=pkatyal@wpi.edu
 #SBATCH --mail-type=ALL
 
 #SBATCH -J p2b
-#SBATCH --output=/home/hkortus/logs/p2b_%j.out
-#SBATCH --error=/home/hkortus/logs/p2b_%j.err
+#SBATCH --output=/home/pkatyal/logs/p2b_%j.out
+#SBATCH --error=/home/pkatyal/logs/p2b_%j.err
 
 #SBATCH -N 1
 #SBATCH -n 8
 #SBATCH --mem=64G
 #SBATCH --gres=gpu:1
 #SBATCH -C H100|A100|V100|A30
-#SBATCH -p academic 
+#SBATCH -p academic
 #SBATCH -t 2:00:00
 
 module load cuda/12.4.0/3mdaov5
-module load miniconda3
 module load ffmpeg/6.1.1/cup2q2r
 
-source "$("conda" info --base)/etc/profile.d/conda.sh"
-conda activate /home/hkortus/.conda/envs/aerial_robotics
+module load miniconda3
 
-python3 /home/hkortus/RBE595/HandsOnAutonomousAerialVehicles/2b_project/group8_p2b/main.py maps/mapSplat.txt
+source "$("conda" info --base)/etc/profile.d/conda.sh"
+conda activate /home/pkatyal/.conda/envs/aerial_robotics
+
+python3 /home/pkatyal/HandsOnAutonomousAerialVehicles/2b_project/group8_p2b/main.py maps/mapSplat.txt
 
