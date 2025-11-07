a217ea7bb3c53b46959e3aa78ac55e563007b06e
Hudson Kortus <hkortus@login-02.cm.cluster>
2025-11-06 22:27:02 -0500

    changed some stuff, idk if i trust it tho, feel free to revert

--- 3_project/group8_p3/Code/window_segmentation/Params.py
+++ 3_project/group8_p3/Code/window_segmentation/Params.py
@@ -1,15 +1,15 @@
 import os
 
 HOME_PATH   =   os.path.expanduser("~")
-JOB_ID      =   "window_seg_small_dataloader_2_1"
+JOB_ID      =   "window_seg_2_new_datalaoder"
 MODEL_NAME  =   "windowseg"
 DS_PATH     =   "/home/hkortus/scratch/window_images/"
 OUT_PATH    =   "/home/hkortus/RBE595/HandsOnAutonomousAerialVehicles/3_project/group8_p3/Code/window_segmentation/logs/"
-PRETRAINED_PATH = "/home/hkortus/RBE595/HandsOnAutonomousAerialVehicles/3_project/group8_p3/Code/window_segmentation/logs/window_seg_small_dataloader_2_1/parameter0/21.pth"
+PRETRAINED_PATH = "/home/hkortus/RBE595/HandsOnAutonomousAerialVehicles/3_project/group8_p3/Code/window_segmentation/logs/window_seg_small_dataloader_2_1/parameter3/19.pth"
 
 JOB_FOLDER  =   os.path.join(OUT_PATH, JOB_ID)
 BATCH_SIZE          =   32
-LR                  =   5e-6
+LR                  =   4e-6
 LOG_BATCH_INTERVAL  =   1
 LOG_WANDB = True
 NUM_WORKERS  =   20
